f0e2670935516915bdcac5059f4ba375da18758b
Keith Potempa <keithrpotempa@gmail.com>
2020-02-03 13:40:59 -0600

    completed exercise

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uran
     to a section element in your HTML with an id of "planets".
     Use string templates to construct the DOM elements.
 */
+
 const planetEl = document.getElementById("planets")
 
+planets.forEach(planet => {    
+    planetEl.innerHTML += `
+        <article id=${planet}>
+            <h2>${planet}</h2>    
+        </article>
+    `
+})
 /*
     Use the map method to create a new array where the
     first letter of each planet is capitalized. Use the
@@ -14,7 +22,12 @@ const planetEl = document.getElementById("planets")
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
+const planetsUpper = planets.map(planet => {
+    const firstLetter = planet.charAt(0);
+    return planet.replace(firstLetter, firstLetter.toUpperCase());
+})
 
+console.log("Planets with first letter in Uppercase: ", planetsUpper)
 
 /*
     Use the filter method to create a new array that
@@ -23,3 +36,7 @@ const planetEl = document.getElementById("planets")
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
+
+const planetsE = planets.filter(planet => planet.includes('e'));
+
+console.log("Planets with the letter 'E' in them: ", planetsE)
